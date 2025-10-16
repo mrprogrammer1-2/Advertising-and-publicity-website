edba858ad7f2ba8495b8b3d771b7f197a66f65fc
mrprogrammer1-2 <sakatag681@gmail.com>
2025-10-16 21:35:18 +0300

    why not

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Services from "./sections/Services";
-// import ServicesMobile from "./sections/ServicesMobile";
 import Testimonial from "./sections/Testimonial";
 import Portfolio from "./sections/Portfolio";
 import MobileServices from "./sections/MobileServices";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
-
-// import MobileServices from "./MobileServices";
-
-// const CombinedServices = () => (
-//   <>
-//     <div className="hidden md:block">
-//       <Services />
-//     </div>
-//     <div className="block md:hidden">
-//       <MobileServices />
-//     </div>
-//   </>
-// );
-
-// export default CombinedServices;
+import Header from "./components/Header";
 
 function App() {
   return (
     <main className="min-h-dvh w-screen">
+      <Header />
       <Hero />
       <About />
       <div className="hidden md:block">
---
+++ src/components/Header.jsx
@@ -0,0 +1,144 @@
+import { useEffect, useRef, useState } from "react";
+import { useWindowScroll } from "react-use";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Header = () => {
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const { y: currentScrollY } = useWindowScroll();
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    if (currentScrollY === 0) {
+      setIsHeaderVisible(true);
+      headerRef.current.classList.remove("floating-nav");
+      console.log("hello");
+    } else if (currentScrollY > lastScrollY) {
+      setIsHeaderVisible(false);
+      headerRef.current.classList.add("floating-nav");
+    } else if (currentScrollY < lastScrollY) {
+      setIsHeaderVisible(true);
+      headerRef.current.classList.add("floating-nav");
+    }
+    setLastScrollY(currentScrollY);
+  }, [currentScrollY]);
+
+  useGSAP(
+    () => {
+      gsap.to(headerRef.current, {
+        y: isHeaderVisible ? 0 : -100,
+        opacity: isHeaderVisible ? 1 : 0,
+        duration: 0.2,
+      });
+    },
+    { dependencies: [isHeaderVisible] }
+  );
+
+  return (
+    <header
+      ref={headerRef}
+      id="header"
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6 px-4"
+    >
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex-1 md:flex md:items-center md:gap-12">
+            <a className="block text-teal-600" href="#">
+              <span className="sr-only">Home</span>
+              <img
+                src="/images/logo.svg"
+                alt="pixel & pulse"
+                className="w-32 max-h-full"
+              />
+            </a>
+          </div>
+
+          <div className="md:flex md:items-center md:gap-12">
+            <nav aria-label="Global" className="hidden md:block">
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="#home"
+                  >
+                    Home
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="#about"
+                  >
+                    About
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="#services"
+                  >
+                    Services
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="#portfolio"
+                  >
+                    Portfolio
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="#contact"
+                  >
+                    Contact
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="#testimonial"
+                  >
+                    Testimonials
+                  </a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="flex items-center gap-4">
+              <div className="block md:hidden">
+                <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
